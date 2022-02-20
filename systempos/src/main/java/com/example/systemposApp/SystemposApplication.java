75e13a371a2b4596cfe98e8c85f7b1babcd96714
ItsMario25 <mmariohernandez58@gmail.com>
2022-02-20 18:45:36 -0500

    Initial Commit

---
+++ systempos/src/main/java/com/example/systemposApp/SystemposApplication.java
@@ -0,0 +1,13 @@
+package com.example.systemposApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemposApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemposApplication.class, args);
+	}
+
+}
---
+++ systempos/src/test/java/com/example/systemposApp/SystemposApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.systemposApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemposApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
